6a7c0617fd91c25f61e5b88d2b81f65ac6886203
Girish <92037937+Giri31@users.noreply.github.com>
2021-11-28 17:20:05 +0530

    Add files via upload

---
+++ string replacement.py
@@ -0,0 +1,22 @@
+def new_string(str):
+  if len(str) >= 2 and str[:2] == "Is":
+    return str
+  return "Is" + str
+
+print(new_string("Array"))
+print(new_string("IsEmpty"))
+
+
+
+
+def change_char(str1):
+  char = str1[0]
+  str1 = str1.replace(char, '$')
+  str1 = char + str1[1:]
+
+  return str1
+
+print(change_char('google'))
+
+
+
